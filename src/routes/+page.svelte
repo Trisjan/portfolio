35e9e0edbef10196acc032fed0c16891a693b080
Trisjan Mustamu <TrisjanM@hotmail.com>
2025-02-27 20:59:25 +0100

    Data fetched

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,48 @@
 <script>
+import { onMount } from "svelte";
+
+const GithubAPI = 'https://api.github.com/users/';
+const Username = 'Trisjan';
+const RepoAPI = '/repos';
+const ApiUrl = `${GithubAPI}${Username}${RepoAPI}`
+
+let repos = [];
+
+onMount(async () => {
+	try {
+            const response = await fetch(ApiUrl);
+            repos = await response.json();
+            
+            repos.sort((a, b) => b.stargazers_count - a.stargazers_count);
+        } catch (error) {
+            console.error('Error fetching repos:', error);
+        }
+})
 
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Portfolio</title>
+	<meta name="description" content="Trisjan's portfolio" />
 </svelte:head>
 
 <section>
 	<h1>
 		Welcome to your new<br />SvelteKit app
 	</h1>
+</section>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>x
+<section class="middle">
+	<h2>Repositories</h2>
+	<div class="repos">
+		{#each repos as repo}
+		<div class="repository">
+			<h2><a href={repo.html_url} target="_blank">{repo.name}</a></h2>
+			<img src={repo.owner.avatar_url} width="50" height="50">
+			<p>{repo.owner.login}</p>
+		</div>
+		{/each}
+	</div>
 </section>
 
 <style>
